9b4cdfa11b4dec1bc0c9578f814b060353cab0c0
trisolidus <73290947+trisolidus@users.noreply.github.com>
2020-10-25 14:13:29 +0700

    Add edit links
    ref
    https://vuepress.vuejs.org/theme/default-theme-config.html#git-repository-and-edit-links

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Discord', link: 'https://discord.com/invite/qayPpkG' },
+    ],
+    repo: 'DevForThaiFreedom/devforthaifreedom',
+    editLinks: true,
+  }
+}
